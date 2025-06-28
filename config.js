6317a298b269ab644f26d6ff24a297852f5650e0
Corex_Anthony <desireanthony24@gmail.com>
2025-06-28 07:28:33 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,9 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const config = {
-  DATABASE_URL: process.env.DATABASE_URL,
-  RENDER_API_KEY: process.env.RENDER_API_KEY,
-  SESSION_ID: process.env.SESSION_ID,
-  PORT: process.env.PORT,
-  RENDER_NAME: process.env.RENDER_NAME,
-};
-
-module.exports = config;
+module.exports = {
+  DATABASE_URL: process.env.DATABASE_URL || '',
+  SESSION_ID: process.env.SESSION_ID || 'ANCORE_DEFAULT',
+  PORT: process.env.PORT || 8080,
+  RENDER_API_KEY: process.env.RENDER_API_KEY || '',
+  TIMEZONE: process.env.TIMEZONE || 'Africa/Lagos'
+}
